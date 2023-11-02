c80fe6c74a412065687fe301b7809d7eaeb7f855
Josefin Mikaelsson <josefin.mikaelsson.NET23@edu.edugrade.se>
2023-11-02 12:31:22 +0100

    Add project files.

---
+++ Employee.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace Labb_8__OOP_Generic_Collections
+{
+    internal class Employee
+    {
+        public int Id;
+        public string Name;
+        public string Gender;
+        public double Salary;
+
+        public Employee(int id, string name, string gender, double salary)
+        {
+            Id = id;
+            Name = name;
+            Gender = gender;
+            Salary = salary;
+        }
+
+        public void PrintInfo()
+        {
+            Console.WriteLine($"Namn: {Name}"+$"\nKön: {Gender}"+$"\nLön: {Salary}"+$"\nAnställningsnummer: {Id}");
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,70 @@
+﻿using System.Reflection.Metadata;
+
+namespace Labb_8__OOP_Generic_Collections
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool startPage = true;
+
+            while (startPage == true)
+            {
+                Console.WriteLine("Företags register");
+
+                Console.WriteLine("" +
+                    "\n" +
+                    "\n1. Lägg till ny anställd" +
+                    "\n2. Ta bort anställd" +
+                    "\n3. Öppna alla anställda" +
+                    "\n" +
+                    "\n4. Exit");
+
+                bool successfulParse = int.TryParse(Console.ReadLine(), out int userInput);
+                while (true)
+                {
+                    if (successfulParse)
+                    {
+                        if (userInput == 1)
+                        {
+                            NewEployee();
+                        }
+                        else if (userInput == 3)
+                        {
+
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Du måste göra ett val, 1,2, 3 eller 4[exit]");
+                    }
+                }
+            }       
+        }
+
+        public static void NewEployee()
+        {
+            Stack<Employee> employees = new Stack<Employee>();
+
+            Console.Write("Namn: ");
+            string userName = Console.ReadLine();
+            Console.Write("Kön: ");
+            string userGender = Console.ReadLine();
+            Console.Write("Lön: ");
+            double userSalary = double.Parse(Console.ReadLine());
+            int userId = 10001;
+
+            Console.WriteLine("-------------------------------");
+            Employee.PrintInfo();
+
+            Console.WriteLine("" +
+                "\nTryck enter för att lägga till anställd");
+            Console.ReadKey();
+
+            Employee employee = new Employee(userId, userName, userGender, userSalary);
+            employees.Push(employee);
+
+            userId++;
+        }
+    }
+}
